01cc96e518e4805063f558dd05825827fb9608f8
Ryan Milkovits <rmilkovi@ucsd.edu>
2016-01-30 10:31:35 -0800

    added input fields in flex column on road trip page

--- RoadeeProject/web2py/applications/Roadee/static/js/scripts.js
+++ RoadeeProject/web2py/applications/Roadee/static/js/scripts.js
@@ -35,30 +35,38 @@ function initAutocomplete() {
   });
 
   // Create the search box and link it to the UI element.
-  var input = document.getElementById('pac-input');
-  var searchBox = new google.maps.places.SearchBox(input);
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+  var inputStart = document.getElementById('pac-input-start');
+  var inputEnd = document.getElementById('pac-input-end');
+  var searchBoxStart = new google.maps.places.SearchBox(inputStart);
+  var searchBoxEnd = new google.maps.places.SearchBox(inputEnd);
+  //map.controls[google.maps.ControlPosition.TOP_LEFT].push(inputStart);
+  //map.controls[google.maps.ControlPosition.TOP_LEFT].push(inputEnd);
 
   // Bias the SearchBox results towards current map's viewport.
   map.addListener('bounds_changed', function() {
-    searchBox.setBounds(map.getBounds());
+    searchBoxStart.setBounds(map.getBounds());
   });
 
-  var markers = [];
+   // Bias the SearchBox results towards current map's viewport.
+  map.addListener('bounds_changed', function() {
+    searchBoxEnd.setBounds(map.getBounds());
+  });
+
+  var markersStart = [];
   // Listen for the event fired when the user selects a prediction and retrieve
   // more details for that place.
-  searchBox.addListener('places_changed', function() {
-    var places = searchBox.getPlaces();
+  searchBoxStart.addListener('places_changed', function() {
+    var places = searchBoxStart.getPlaces();
 
     if (places.length == 0) {
       return;
     }
 
     // Clear out the old markers.
-    markers.forEach(function(marker) {
+    markersStart.forEach(function(marker) {
       marker.setMap(null);
     });
-    markers = [];
+    markersStart = [];
 
     // For each place, get the icon, name and location.
     var bounds = new google.maps.LatLngBounds();
@@ -73,6 +81,52 @@ function initAutocomplete() {
 
       // Create a marker for each place.
       markers.push(new google.maps.Marker({
+        map: map,
+        icon: icon,
+        title: place.name,
+        position: place.geometry.location
+      }));
+
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+  });
+
+
+  var markersEnd = [];
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBoxEnd.addListener('places_changed', function() {
+    var places = searchBoxEnd.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    }
+
+    // Clear out the old markers.
+    markersEnd.forEach(function(marker) {
+      marker.setMap(null);
+    });
+    markersEnd = [];
+
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function(place) {
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+
+      // Create a marker for each place.
+      markersEnd.push(new google.maps.Marker({
         map: map,
         icon: icon,
         title: place.name,
