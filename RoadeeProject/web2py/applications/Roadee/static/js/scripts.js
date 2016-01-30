9062b1c160ee78ec5b1f468b05e4c94ac50a44f6
Sidney- <onelookingformusic@gmail.com>
2016-01-29 20:38:05 -0800

    Image added and changes

--- RoadeeProject/web2py/applications/Roadee/static/js/scripts.js
+++ RoadeeProject/web2py/applications/Roadee/static/js/scripts.js
@@ -1,1 +1,29 @@
-alert("Inside");
+/*var map;
+		function initMap() {
+		  map = new google.maps.Map(document.getElementById('map'), {
+			center: {lat: 37.8019, lng: -122.4189},
+			zoom: 15
+		  });
+		}*/
+
+		//Prints Map
+		function initMap() {
+  		var myLatLng = {lat: 37.8019, lng: -122.4189};
+
+  		var map = new google.maps.Map(document.getElementById('map'), {
+    	zoom: 15,
+		mapTypeControl: false,
+    	center: myLatLng
+
+  		});
+
+  		//Adds customer marker
+		var image = "{{=IMG(_src = URL('static','images/RoadeeMarker(Shadow).png'))}}";
+
+		var marker = new google.maps.Marker({
+			position: myLatLng,
+			map: map,
+			icon: image
+  		});
+
+		}
