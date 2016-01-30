4ca451b6d5f89d12615604e538bb8d400caa4bfe
Sidney- <onelookingformusic@gmail.com>
2016-01-30 14:35:36 -0800

    Completed Homepage

--- RoadeeProject/web2py/applications/Roadee/static/js/scripts.js
+++ RoadeeProject/web2py/applications/Roadee/static/js/scripts.js
@@ -6,19 +6,37 @@
 		  });
 		}*/
 
+		var isClicked = true;
+
+		$(document).ready(function(){
+			$("#inside").click(function() {
+				if (isClicked) {
+					$(this).animate({
+						marginTop:"-=35px"}, 200);
+						isClicked = false;
+
+				}else {
+					$(this).animate({
+						marginTop:"+=35px"}, 200);
+						isClicked = true;
+
+				}
+			});
+		});
+
 		//Prints Map
 		function initMap() {
-  		var myLatLng = {lat: 37.8019, lng: -122.4189};
+			var myLatLng = {lat: 37.8019, lng: -122.4189};
 
-  		var map = new google.maps.Map(document.getElementById('map'), {
-    	zoom: 15,
-		mapTypeControl: false,
-    	center: myLatLng
+			var map = new google.maps.Map(document.getElementById('map'), {
+				zoom: 15,
+				mapTypeControl: false,
+				center: myLatLng
+			});
 
-  		});
 
   		//Adds customer marker
-		var image = "{{=IMG(_src = URL('static','images/RoadeeMarker(Shadow).png'))}}";
+		var image = 'static/images/waypoint.png';
 
 		var marker = new google.maps.Marker({
 			position: myLatLng,
