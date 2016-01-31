795e10529248e39bcf6f3b2edfeec48bfa41e18e
Joshua Wallace <jtrwallace@mac.com>
2016-01-30 19:27:31 -0800

    Special Commit

--- RoadeeProject/web2py/applications/Roadee/controllers/default.py
+++ RoadeeProject/web2py/applications/Roadee/controllers/default.py
@@ -11,15 +11,7 @@
 import json
 
 def index():
-    """
-    example action using the internationalization operator T and flash
-    rendered by views/default/index.html or views/generic.html
-
-    if you need a simple wiki simply replace the two lines below with:
-    return auth.wiki()
-    """
-    response.flash = T("Hello World")
-    return dict(message=T('Welcome to web2py!'))
+    return dict(registerForm=auth.register(), loginForm=auth.login())
 
 def roadtrip():
     return dict(message=T('Welcome to web2py!'))
@@ -186,3 +178,6 @@ def update_route():
 
 def update_waypoint():
     return 0
+
+def test():
+    return dict(form=auth.login())
