b7be05e85aa2547da44f033fb8297a627c79f51e
Ryan Milkovits <rmilkovi@ucsd.edu>
2016-01-30 20:11:26 -0800

    removed bugs from roadtrip.html

--- RoadeeProject/web2py/parameters_8800.py
+++ RoadeeProject/web2py/parameters_8800.py
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-password="pbkdf2(1000,20,sha512)$a7b086fec81db803$41a4e7ba13f83633f23f063c491a6dec64f6b5b0"
-=======
-password="pbkdf2(1000,20,sha512)$b489ad8cd1a7c402$6e86f5e9e580c872ad9bdc4bc3f6ec1660d2eb65"
->>>>>>> 992a96edd3554fb721cc56f56bf6435d7d476cf8
+password="pbkdf2(1000,20,sha512)$9cf494206afbcb83$4d651ad82751567fb74bdcbcf6e769b46f4d1e44"
